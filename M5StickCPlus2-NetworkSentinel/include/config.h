f100ef08b8544dd852eed6f68cc793d3cdaddb1b
exai <jakubzak17@proton.me>
2025-11-03 13:55:45 +0100

    Added .gitignore + configuration files/examples

---
+++ M5StickCPlus2-NetworkSentinel/include/config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern const char* WIFI_SSID;
+extern const char* WIFI_PASS;
+
+#endif
---
+++ M5StickCPlus2-NetworkSentinel/include/config_private.example.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_PRIVATE_H
+#define CONFIG_PRIVATE_H
+
+//Example
+const char* WIFI_SSID = "YourWiFiName";
+const char* WIFI_PASS = "YourWiFiPassword";
+
+#endif
