7f2e49f31dd6df0d1e34d57d69b03ef0835974a7
exai <jakubzak17@proton.me>
2025-11-03 16:02:00 +0100

    Added connectivity test
    Added connectivity test and UI addons like colors

--- M5StickCPlus2-NetworkSentinel/M5StickCPlus2-NetworkSentinel.ino
+++ M5StickCPlus2-NetworkSentinel/M5StickCPlus2-NetworkSentinel.ino
@@ -1,23 +1,175 @@
 #include <WiFi.h>
-#include <WiFiAP.h>
-#include <WiFiClient.h>
-#include <WiFiGeneric.h>
 #include <WiFiMulti.h>
-#include <WiFiSTA.h>
-#include <WiFiScan.h>
-#include <WiFiServer.h>
-#include <WiFiType.h>
-#include <WiFiUdp.h>
 #include <M5StickCPlus2.h>
-#include "include/config.h"
-#include "include/config_private.h"
+#include "include/config_private.h" //Change config.h to config_private.h after downloading a file and fill WiFi SSID and password, otherwise 
+
+int currentMode = 0;
+
+void showMenu();
+void runMode(int mode);
+void runConnectivityTest();
 
 void setup() {
   M5.begin();
   M5.Lcd.setTextSize(2);
-  M5.Lcd.println("Hello M5StickCPlus2!");
+  M5.Lcd.setTextColor(WHITE, BLACK);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setRotation(1);
+  M5.Lcd.println("Network Sentinel");
+
+  //Connecting with WiFi provided in private config
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  M5.Lcd.print("Connecting to Wi-Fi");
+   int timeout = 0;
+  while (WiFi.status() != WL_CONNECTED && timeout < 20) { // 20 * 500ms = 10s
+    delay(500);
+    M5.Lcd.print(".");
+    timeout++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    M5.Lcd.println("\nConnected!");
+    M5.Lcd.print("IP: ");
+    M5.Lcd.println(WiFi.localIP());
+  } else {
+    M5.Lcd.println("\nWi-Fi Failed!");
+  }
+
+  delay(1500);
+  showMenu();
 }
 
 void loop() {
   M5.update();
+
+  // Przycisk A zmiana opcji
+  if(M5.BtnA.wasPressed()){
+    currentMode++;
+    if(currentMode>2)currentMode = 0;
+    showMenu();
+  }
+
+  // Przycisk B wybór
+  if(M5.BtnB.wasPressed()){
+    runMode(currentMode);
+    showMenu();
+  }
 }
+
+void showMenu() {
+  //Different colors for modes
+  uint16_t bgColor;
+  uint16_t textColor;
+  uint16_t highlightColor;
+
+  if (currentMode == 0) {
+    bgColor = BLACK;
+    textColor = WHITE;
+    highlightColor = WHITE;
+  } else if (currentMode == 1) {
+    bgColor = TFT_PURPLE;
+    textColor = TFT_ORANGE;
+    highlightColor = TFT_ORANGE;
+  } else if (currentMode == 2) {
+    bgColor = TFT_CYAN;
+    textColor = TFT_NAVY; 
+    highlightColor = TFT_RED;
+  }
+
+  M5.Lcd.fillScreen(bgColor);
+  M5.Lcd.setTextColor(textColor, bgColor);
+
+  M5.Lcd.setCursor(10, 0);
+  M5.Lcd.println("== MENU ==");
+
+  M5.Lcd.setCursor(0, 30);
+
+  if (currentMode == 0) {
+    M5.Lcd.setTextColor(highlightColor, bgColor);
+    M5.Lcd.println("> Network Sentinel");
+    M5.Lcd.setTextColor(textColor, bgColor);
+    M5.Lcd.println("  Config & WiFi");
+    M5.Lcd.println("  Connectivity Test");
+  } else if (currentMode == 1) {
+    M5.Lcd.setTextColor(textColor, bgColor);
+    M5.Lcd.println("  Network Sentinel");
+    M5.Lcd.setTextColor(highlightColor, bgColor);
+    M5.Lcd.println("> Config & WiFi");
+    M5.Lcd.setTextColor(textColor, bgColor);
+    M5.Lcd.println("  Connectivity Test");
+  } else if (currentMode == 2) {
+    M5.Lcd.setTextColor(textColor, bgColor);
+    M5.Lcd.println("  Network Sentinel");
+    M5.Lcd.println("  Config & WiFi");
+    M5.Lcd.setTextColor(highlightColor, bgColor);
+    M5.Lcd.println("> Connectivity Test");
+  }
+}
+
+
+void runMode(int mode) {
+  switch (mode) {
+    case 0:
+      M5.Lcd.fillScreen(BLACK);
+      M5.Lcd.println("Network Sentinel");
+      // TODO: Main function that will scan network like nmap
+      delay(2000);
+      break;
+
+    case 1:
+      M5.Lcd.fillScreen(BLACK);
+      M5.Lcd.println("WiFi config...");
+      // TODO: Function that show wifi configuration, private ip address
+      delay(2000);
+      break;
+
+    case 2:
+      runConnectivityTest();
+      break;
+  }
+}
+
+// =============================
+// Mode 2: Connectivity Test
+// =============================
+// It works by checking connection with Google DNS(8.8.8.8 on port 53[DNS])
+void runConnectivityTest() {
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.println("Connectivity Test\n");
+
+  if (WiFi.status() != WL_CONNECTED) {
+    M5.Lcd.println("Wi-Fi: Not connected");
+    delay(5000);
+    return;
+  }
+  delay(1000);
+  M5.Lcd.print("IP: ");
+  M5.Lcd.println(WiFi.localIP());
+  M5.Lcd.println("\nTesting...");
+
+  WiFiClient client;
+  unsigned long start = millis();
+
+  if (client.connect("8.8.8.8", 53)) {
+    unsigned long elapsed = millis() - start;
+    M5.Lcd.setTextColor(TFT_GREEN, BLACK);
+    M5.Lcd.println("Internet: OK");
+    M5.Lcd.print("Latency: ");
+    M5.Lcd.print(elapsed);
+    M5.Lcd.println(" ms");
+    client.stop();
+    delay(15000);
+  } else {
+    M5.Lcd.setTextColor(TFT_RED, BLACK);
+    M5.Lcd.println("Internet: FAIL");
+    delay(5000);  
+  }
+
+  M5.Lcd.println("\nPress B to return to menu");
+  while (!M5.BtnB.wasPressed()) {
+    M5.update();
+    delay(50);
+  }
+}
+
